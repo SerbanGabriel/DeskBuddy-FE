0ce9e0e032edc986e274ebe66df4361a2ab4dcf2
sgaby100 <sgaby100@gmail.com>
2024-02-16 02:20:59 +0200

    added admin page

--- src/app/Components/admin/admin.component.ts
+++ src/app/Components/admin/admin.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatInputModule,MatButtonModule,ReactiveFormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  itemForm = this.fb.group({
+    productTitle:'',
+    productDescription:'',
+    productPrice:'',
+    productCount:''
+  })
+  constructor(private fb:FormBuilder){
+
+  }
+
+  saveItem(){
+    
+  }
 
 }
